4347973df8cd423e76ae0fc93fde19c27e2bb233
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-02-24 17:31:13 -0500

    213. House Robber II

---
+++ 213-house-robber-II.java
@@ -0,0 +1,41 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+*/
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        return Math.max(rob0(nums), rob1(nums));
+    } 
+
+    public int rob0(int[] nums) {
+        int prevMax = 0;
+        int currMax = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + nums[i], currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+
+    public int rob1(int[] nums) {
+        int prevMax = 0;
+        int currMax = 0;
+        for(int i = 1; i < nums.length; i++) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + nums[i], currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
