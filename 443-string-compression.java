54bcc56033c1c674ae4fbc5083c0601feef60e3d
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-03-19 13:39:51 -0400

    443. String Compression

--- 443-string-compression.java
+++ 443-string-compression.java
@@ -18,3 +18,22 @@ Output: Return 6, and the first 6 characters of the input array should be: ["a",
 Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
  */
 
+ class Solution {
+    public int compress(char[] chars) {
+        int i = 0, res = 0;
+        while(i < chars.length) {
+            int groupLength = 1;
+            while(i + groupLength < chars.length && chars[i + groupLength] == chars[i]) {
+                groupLength++;
+            }
+            chars[res++] = chars[i];
+            if(groupLength > 1) {
+                for(char c : Integer.toString(groupLength).toCharArray()) {
+                    chars[res++] = c;
+                }
+            }
+            i += groupLength;
+        }
+        return res;
+    }
+}
