e7c8214609f7d2d30bfbc4ca74a0fa1ce797b62d
AnastasiiaBoryslavska <anastasiiaforwork@gmail.com>
2023-04-09 18:46:28 -0400

    908. Smallest Range I

---
+++ 908-smallest-range-I.java
@@ -0,0 +1,20 @@
+/*
+You are given an integer array nums and an integer k.
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k].
+ You can apply this operation at most once for each index i.
+The score of nums is the difference between the maximum and minimum elements in nums.
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+ */
+
+public class Solution {
+    public int smallestRangeI(int[] arr, int k) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        min = (max - k) - (min + k);
+        return min < 0 ? 0 : min;
+    }
+}
