681bcf35cf269c42779934d2a6b33b5c72c324df
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-07 21:12:29 -0400

    145. Binary Tree Postorder Traversal

---
+++ 145-binary-tree-postorder-traversal.js
@@ -0,0 +1,42 @@
+/**
+ Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// Iterative Solution
+var postorderTraversal = function(root) {
+    if (!root) return [];
+    let stack = [], res = [];
+    stack.push(root);
+    while (stack.length) {
+        let node = stack[stack.length-1];
+        if (node.left) {
+            stack.push(node.left);
+            node.left = null;
+        }
+        else if (node.right) {
+            stack.push(node.right);
+            node.right = null;
+        }
+        else res.push(stack.pop().val);
+    }
+    return res;
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+};
