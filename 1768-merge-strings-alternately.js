9f5e893c675c26ca473c28a233aa8ac3949650d7
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-19 21:36:20 -0400

    1768. Merge Strings Alternately

---
+++ 1768-merge-strings-alternately.js
@@ -0,0 +1,28 @@
+/**
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+*/
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let result = "";
+    for(let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if(i < word1.length) result += word1[i];
+        if(i < word2.length) result += word2[i];
+    }
+    return result;
+};
