b9d56ac8e70f7df46bbb70233a38872334d09b81
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-22 14:33:24 -0400

    637. Average of Levels in Binary Tree

---
+++ 637-average-of-levels-in-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [3.00000,14.50000,11.00000]
+Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+Hence return [3, 14.5, 11].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    let q = [root], ans = []
+    while (q.length) {
+        let qlen = q.length, row = 0
+        for (let i = 0; i < qlen; i++) {
+            let curr = q.shift()
+            row += curr.val
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
+        }
+        ans.push(row/qlen)
+    }
+    return ans
+};
