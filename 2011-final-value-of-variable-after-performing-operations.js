5a8cb85af8738ec404fddc3a8dfcb4c7459bfbc6
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-24 19:36:08 -0400

    2011. Final Value of Variable After Performing Operations

---
+++ 2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,36 @@
+/**
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+ 
+*/
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+const finalValueAfterOperations = (operations) => {
+    let x = 0;
+    for (let i = 0; i < operations.length; i++) {
+      if (operations[i] === "X++" || operations[i] === "++X") {
+        x++;
+      } else {
+        x--;
+      }
+    }
+    return x;
+  };
