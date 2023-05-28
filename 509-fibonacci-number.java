02160a57c2ef7e6d7314afcf8f7f532aeaa31710
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-28 14:46:14 -0400

    509. Fibonacci Number

---
+++ 509-fibonacci-number.java
@@ -0,0 +1,24 @@
+/**
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+*/
+
+class Solution 
+{
+    public int fib(int N)
+    {
+        if(N <= 1)
+            return N;
+        else
+            return fib(N - 1) + fib(N - 2);
+    }
+}
