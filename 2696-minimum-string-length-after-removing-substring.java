2b3433d7dd45daa8651f96dfd577c7efa22b4c5d
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-03 16:54:33 -0400

    2696. Minimum String Length After Removing Substrings

---
+++ 2696-minimum-string-length-after-removing-substring.java
@@ -0,0 +1,38 @@
+/**
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+Example 1:
+
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain. 
+*/
+
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        st.push(s.charAt(0));
+        for(int i=1;i<n;i++)
+        {
+            if(!st.isEmpty()&&((s.charAt(i)=='B'&&st.peek()=='A')||(s.charAt(i)=='D'&&st.peek()=='C')))
+            {
+                st.pop();
+            }
+            else
+                st.push(s.charAt(i));
+        }
+        return st.size();
+    }
+}
