68bc017e9fd99661438eecb21901454938ea54d5
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-10 12:06:42 -0400

    2706. Buy Two Chocolates

---
+++ 2706-uy-two-chocolates.java
@@ -0,0 +1,19 @@
+/**
+ You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+
+You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
+
+Example 1:
+
+Input: prices = [1,2,2], money = 3
+Output: 0
+Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
+*/
+
+class Solution {
+    public int buyChoco(int[] a, int money) {
+        Arrays.sort(a); int k=a[0]+a[1];if(k<=money) return money-k; return money;
+    }
+}
