3b5bbd32e862c412e59c6e8ff24a6ac53be2bdbf
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-12 14:17:11 -0400

    680. Valid Palindrome II

---
+++ 680-valid-palindrome-||.java
@@ -0,0 +1,34 @@
+/**
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+ */
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
