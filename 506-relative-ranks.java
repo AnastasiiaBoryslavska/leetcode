8d55f5cbc07177734bc63ec48b93149e1b3533ae
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-15 14:38:55 -0400

    506. Relative Ranks

---
+++ 506-relative-ranks.java
@@ -0,0 +1,49 @@
+/**
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+
+Example 1:
+
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+*/
+
+class Solution {
+    public String[] findRelativeRanks(int[] nums) {
+        Integer[] index = new Integer[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            index[i] = i;
+        }
+        
+        Arrays.sort(index, (a, b) -> (nums[b] - nums[a]));
+        
+        String[] result = new String[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                result[index[i]] = "Gold Medal";
+            }
+            else if (i == 1) {
+                result[index[i]] = "Silver Medal";
+            }
+            else if (i == 2) {
+                result[index[i]] = "Bronze Medal";
+            }
+            else {
+                result[index[i]] = (i + 1) + "";
+            }
+        }
+
+        return result;
+    }
+}
