d44b6366cebdd1dbffc6ecef7dc58ea69e1a22f9
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-16 12:42:19 -0400

    520. Detect Capital

---
+++ 520-detect-capital.java
@@ -0,0 +1,22 @@
+/**
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+*/
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() < 2) return true;
+        if (word.toUpperCase().equals(word)) return true;
+        if (word.substring(1).toLowerCase().equals(word.substring(1))) return true;
+        return false;
+    }
+}
