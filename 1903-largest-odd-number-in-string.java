ed71b102131931ece7ec29d0418239bc71237655
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-06-29 20:32:55 -0400

    1903. Largest Odd Number in String

---
+++ 1903-largest-odd-number-in-string.java
@@ -0,0 +1,20 @@
+/**
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+*/
+
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i > -1; i--) {
+            if (num.charAt(i) % 2 == 1) return num.substring(0,i+1);
+        }
+        return "";
+    }
+}
