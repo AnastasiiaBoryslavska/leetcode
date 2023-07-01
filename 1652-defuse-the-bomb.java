2992cb47c43bd0dd30e8b14a40b96c2ce447e4d0
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-07-01 12:17:54 -0400

    1652. Defuse the Bomb

---
+++ 1652-defuse-the-bomb.java
@@ -0,0 +1,40 @@
+/**
+You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+
+To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous k numbers.
+If k == 0, replace the ith number with 0.
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+
+Example 1:
+
+Input: code = [5,7,1,4], k = 3
+Output: [12,10,16,13]
+Explanation: Each number is replaced by the sum of the next 3 numbers. The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
+*/
+
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int[] res = new int[code.length];
+        if (k == 0) return res;
+        //Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {//If k < 0, the starting point will be end of the array.
+            k = -k;
+            start = code.length - k;
+            end = code.length - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        //Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            res[i] = sum;
+            sum -= code[(start++) % code.length];
+            sum += code[(++end) % code.length];
+        }
+        return res;
+    }
+}
