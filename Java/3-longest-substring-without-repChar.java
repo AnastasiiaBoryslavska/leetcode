d944e6ad8e6afe75c19c43f7852596aea60da31f
AnastasiiaBoryslavska <anastasiiaforwork@gmail.com>
2025-05-31 23:29:48 -0400

    Reordering files

--- Java/3-longest-substring-without-repChar.java
+++ Java/3-longest-substring-without-repChar.java
@@ -1,8 +1,11 @@
+import java.util.Set;
+import java.util.HashSet;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         //define pointers, maxLen, hashSet
         int l = 0, r = 0, maxLen = 0;
-        HashSet<Character> hs = new HashSet<>();
+        Set<Character> hs = new HashSet<>();
         //check if character at r pointer exists in the hashSet, if not - add
         while(r < s.length()) {
             if(!hs.contains(s.charAt(r))) {
