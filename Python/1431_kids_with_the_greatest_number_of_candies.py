1df7384c4d3aa1cebafb3c98f926d814009c2f90
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2025-06-11 12:44:19 -0400

    1431. Kids With the Greatest Number of Candies

---
+++ Python/1431_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,26 @@
+#There are n kids with candies. You are given an integer array candies, where each candies[i] represents the
+#number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+#Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies,
+#they will have the greatest number of candies among all the kids, or false otherwise.
+#Note that multiple kids can have the greatest number of candies.
+
+#Example 1:
+#Input: candies = [2,3,5,1,3], extraCandies = 3
+#Output: [true,true,true,false,true] 
+#Explanation: If you give all extraCandies to:
+#- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+#- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+#- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+#- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+#- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        # Find out the greatest number of candies among all the kids.
+        maxCandies = max(candies)
+        # For each kid, check if they will have greatest number of candies
+        # among all the kids.
+        result = []
+        for i in range(len(candies)):            
+            result.append(candies[i] + extraCandies >= maxCandies)
+        return result
