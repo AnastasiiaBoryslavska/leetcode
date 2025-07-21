160510c960f46960811947a10ea30335797c4810
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2025-07-21 15:20:23 -0400

    700. Search in a Binary Search Tree

---
+++ Python/700_search in_a_binary_search_tree.py
@@ -0,0 +1,21 @@
+#You are given the root of a binary search tree (BST) and an integer val.
+#Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+        
