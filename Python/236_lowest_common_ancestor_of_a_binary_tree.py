576a97a4a525d8dccaf6ba3978bf3970fd19b942
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2025-07-26 15:17:56 -0400

    236. Lowest Common Ancestor of a Binary Tree

---
+++ Python/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,45 @@
+'''
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+				# If looking for me, return myself
+        if root == p or root == q:
+            return root
+        
+        left = right = None
+        # else look in left and right child
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+
+        # if both children returned a node, means
+        # both p and q found so parent is LCA
+        if left and right:
+            return root
+        else:
+        # either one of the chidren returned a node, meaning either p or q found on left or right branch.
+        # Example: assuming 'p' found in left child, right child returned 'None'. This means 'q' is
+        # somewhere below node where 'p' was found we dont need to search all the way, 
+        # because in such scenarios, node where 'p' found is LCA
+            return left or right
+        
