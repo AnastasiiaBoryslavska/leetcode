d9a3df59b59cc83da9f2e953f1ead50f5c7293bc
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2025-07-27 14:08:27 -0400

    199. Binary Tree Right Side View

---
+++ Python/199_binary_tree_right_side_view.py
@@ -0,0 +1,34 @@
+'''
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        result = []
+        
+        if not root:
+            return result
+        q = deque()
+        
+        q.append(root)
+        
+        while q:
+            level_len = len(q)
+            
+            for _ in range(level_len):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            result.append(node.val)
+        return result
+    
